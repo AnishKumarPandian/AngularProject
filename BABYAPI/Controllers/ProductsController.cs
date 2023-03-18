96259bf978925261f6c64a41891758486fc845e9
Anishkumar.p@saint-gobain.com <anishkumar.p@saint-gobain.com>
2023-03-18 23:20:06 +0530

    IReposistory added & SeedData

--- BABYAPI/Controllers/ProductsController.cs
+++ BABYAPI/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using infrastructure.Data;
 using core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using core.Interfaces;
 
 namespace BABYAPI.Controllers;
 
@@ -9,21 +10,18 @@ namespace BABYAPI.Controllers;
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
-    private readonly StoreContext _context;
-    public ProductsController(StoreContext context)
-    {
-        _context = context;
-    }
-    [HttpGet("GetProducts")]
-    public ActionResult<List<Product>> GetProducts()
-    {
-        return Ok(_context.Products.ToList());
-    }
+    private readonly IProductRepository _repo;
 
-    [HttpGet("GetAsyncProducts")]
-    public async Task<ActionResult<List<Product>>> GetAsyncProducts()
+    public ProductsController(IProductRepository repo)
     {
-        var product = await _context.Products.ToListAsync();
+        _repo = repo;
+    }
+   
+
+    [HttpGet("GetProducts")]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var product = await _repo.GetProductsAsync();
         return Ok(product);
     }
 
@@ -31,6 +29,20 @@ public class ProductsController : ControllerBase
     public async Task<ActionResult<List<Product>>> GetProduct(int id)
     {
 
-        return Ok(await _context.Products.FindAsync(id));
+        return Ok(await _repo.GetProductByIdAsync(id));
+    }
+
+    [HttpGet("GetProductBrands")]
+    public async Task<ActionResult<List<Product>>> GetProductBrands()
+    {
+        var productbrands = await _repo.GetProductBrandsAsync();
+        return Ok(productbrands);
+    }
+
+    [HttpGet("GetProductTypes")]
+    public async Task<ActionResult<List<Product>>> GetProductTypes()
+    {
+        var producttypes = await _repo.GetProductTypesAsync();
+        return Ok(producttypes);
     }
 }
--- BABYAPI/Program.cs
+++ BABYAPI/Program.cs
@@ -1,9 +1,11 @@
+using core.Interfaces;
+using infrastructure;
 using infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);  
 
@@ -13,12 +15,20 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        
 
        // var connectionString = Configuration["PostgreSql:ConnectionString"];
         builder.Services.AddDbContext<StoreContext>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("LoginConnection")));
 
-
+        IServiceCollection serviceCollection = builder.Services.AddScoped<IProductRepository, ProductRespository>();
         var app = builder.Build();
+
+        using(var scope = app.Services.CreateScope()){
+
+            var db = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            db.Database.Migrate();
+            await StoreContextSeed.SeedAsync(db);         
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
